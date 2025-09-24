55edfd5fe419d9aabddb4e098d5d5efcad9649db
AakrshitThakur <thakurraakrshitt@gmail.com>
2025-09-24 13:59:48 +0530

    tmp folder paths changed on different environments

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,8 +1,13 @@
 import multer, { type FileFilterCallback } from "multer";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const node_env = process.env.NODE_ENV;
 
 const uploadClothImages = multer({
-  dest: "tmp/", // Vercel allows writing here
+  dest: node_env === "development" ? "tmp/" : "/tmp", // Vercel allows writing here
   limits: { fileSize: 5 * 1024 * 1024 },
   // @params: file -  Object containing information about the processed file
   // cb - a function to control which files should be uploaded and which should be skipped.
